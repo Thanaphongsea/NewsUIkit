5138d105ccf4c10b5365ca28dbba11961b200a46
Thana-phong <xea.thanaphong@gmail.com>
2024-02-07 05:22:17 +0700

    Initial Commit

---
+++ NewsUIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsUIkit
+//
+//  Created by ธนพงษ์ แจ้งสว่าง on 7/2/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
